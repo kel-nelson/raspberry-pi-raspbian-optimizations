4ed9f5ee36d3621877fb815177c34e24c85658eb
kel-nelson <ukcn001@gmail.com>
2014-12-03 23:06:12 -0600

    Update bluetooth-sixaxis.sh

--- bluetooth-sixaxis.sh
+++ bluetooth-sixaxis.sh
@@ -1,7 +1,7 @@
 sudo apt-get install bluez-utils bluez-compat bluez-hcidump checkinstall libusb-dev  libbluetooth-dev joystick
 wget http://www.pabr.org/sixlinux/sixpair.c 
 gcc -o sixpair sixpair.c -lusb
-wget http://sourceforge.net/projects/qtsixa/files/QtSixA%201.5.1/QtSixA-1.5.1-src.tar.gz
+wget http://downloads.sourceforge.net/project/qtsixa/QtSixA%201.5.1/QtSixA-1.5.1-src.tar.gz?r=&ts=1417669422&use_mirror=waia
 tar xfvz QtSixA-1.5.1-src.tar.gz
 wget https://bugs.launchpad.net/qtsixa/+bug/1036744/+attachment/3260906/+files/compilation_sid.patch
 patch ~/QtSixA-1.5.1/sixad/shared.h < compilation_sid.patch
